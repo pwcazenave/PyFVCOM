28a75918c951774c4e47c7129c121f527b3692d9
Pierre Cazenave <pwcazenave@gmail.com>
2012-05-11 16:48:30 +0100

    Code actually works now and will sample an arbitrary number of points from a grid with pretty colours and everything

--- tidal_results.py
+++ tidal_results.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-def extractTideElevation(data, elementIdx, noisy=False):
+import plot_unstruct_grid as gp
+from readFVCOM import readFVCOM
+
+
+def extractTideElevation(data, elementIdx):
     """
     Extract the tidal elevation only (no water depth) from an element (or
     range of elements) and return as arrays for plotting or further analysis.
 
     Give input data as 2D array (location and time). Specify the indices to
-    extract the tidal elevation. Optionally give noisy=True to turn on
-    information during processing.
+    extract the tidal elevation.
 
     Specify the elements as an array (even if there's only one).
 
@@ -23,6 +29,11 @@ def extractTideElevation(data, elementIdx, noisy=False):
     numSteps, numElem = np.shape(data)
     numStations = np.shape(elementIdx)[0]
     output = np.zeros((numStations, numSteps)) * np.nan
+
+    # Extract the time series for each element requested
+    output = data[:,elementIdx]
+
+    return output
 
 
 if __name__ == '__main__':
@@ -44,29 +55,43 @@ if __name__ == '__main__':
     FVCOM = readFVCOM(in1, getVars, noisy)
     [triangles, nodes, x, y, z] = gp.parseUnstructuredGridFVCOM(in2)
 
-    # Set the leak index point (one less than MATLAB)
-    if 'V5' in in1:
-        leakIdx = 1315
-    elif 'V7' in in1:
-        leakIdx = 8186
-    elif 'S5' in in1:
-        leakIdx = 1315
-    elif 'S7' in in1:
-        leakIdx = 8186
-    else:
-        # Could be either...
-        leakIdx = 1315
-        #leakIdx = 8186
-
-    # Sigma layer
-    layerIdx = 0
-
-    # Start index for each input file
-    #startIdx = 770 # Riqui's
-    startIdx = 120 # Mine
-
-    # Skip value for animation (not zero)
-    skipIdx = 5
+    # Number of subplots
+    numPlots = 5
 
     dt = (FVCOM['time'][1]-FVCOM['time'][0])*60*60*24
+
+    samplingIdx = [174, 259, 3] # start, end, skip
+    positionIdx = np.arange(samplingIdx[0], samplingIdx[1], samplingIdx[2])
+    skippedIdx = np.arange(samplingIdx[0], samplingIdx[1], samplingIdx[2] * numPlots)
+
+    try:
+        Z = FVCOM['zeta']
+    except:
+        print 'Did not find tidal elevation (zeta) in model output'
+
+    tidalHeights = extractTideElevation(Z, positionIdx)
+
+    t = FVCOM['time']-np.min(FVCOM['time']) # start time at zero
+
+    plt.figure(1)
+    plt.clf()
+    for i in xrange(numPlots):
+        plt.subplot(numPlots,1,i)
+        colourIdx = int((i/float(numPlots))*255)
+        plt.plot(t, Z[:, skippedIdx[i]], '-x', label='Station 0', color=cm.rainbow(colourIdx))
+        plt.axis('auto')
+
+
+    # Plot figure of the locations of the extracted points
+    gp.plotUnstructuredGrid(triangles, nodes, x, y, FVCOM['zeta'][-1,:], 'Tidal elevation (m)')
+    plt.plot(x[positionIdx], y[positionIdx], '.')
+    for i in positionIdx:
+        plt.text(x[i], y[i], str(i),
+            horizontalalignment='center', verticalalignment='center', size=8)
+
+    for i in skippedIdx:
+        plt.text(x[i], y[i], str(i),
+            horizontalalignment='center', verticalalignment='center', size=18)
+        
+
 
