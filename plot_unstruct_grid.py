d1eb4dd4620d82576892e156ca1529f499093af9
Pierre Cazenave <pwcazenave@gmail.com>
2012-05-30 16:42:24 +0100

    Add support for reading MIKE unstructured grid files

--- plot_unstruct_grid.py
+++ plot_unstruct_grid.py
@@ -7,7 +7,7 @@ import numpy as np
 import math
 
 def parseUnstructuredGridSMS(mesh):
-    """ Reads in the output of SMS Mesh generation. """
+    """ Reads in the SMS unstructured grid format. """
 
     fileRead = open(mesh, 'r')
     lines = fileRead.readlines()
@@ -43,7 +43,7 @@ def parseUnstructuredGridSMS(mesh):
     return(triangle, nodes, X, Y, Z)
 
 def parseUnstructuredGridFVCOM(mesh):
-    """ Reads in the unstructured grid input for FVCOM. """
+    """ Reads in the FVCOM unstructured grid format. """
 
     fileRead = open(mesh, 'r')
     # Skip the file header (two lines)
@@ -78,6 +78,45 @@ def parseUnstructuredGridFVCOM(mesh):
 
     return(triangle, nodes, X, Y, Z)
 
+def parseUnstructuredGridMIKE(mesh):
+    """ Reads in the MIKE unstructured grid format. """
+
+    fileRead = open(mesh, 'r')
+    # Skip the file header (one line)
+    lines = fileRead.readlines()[1:]
+    fileRead.close()
+
+    triangles = []
+    nodes = []
+    x = []
+    y = []
+    z = []
+
+    for line in lines:
+        ttt = line.strip().split()
+        if len(ttt) == 4:
+            t1 = int(ttt[1])-1
+            t2 = int(ttt[2])-1
+            t3 = int(ttt[3])-1
+            triangles.append([t1, t2, t3])
+        elif len(ttt) == 5:
+            x.append(float(ttt[1]))
+            y.append(float(ttt[2]))
+            z.append(float(ttt[3]))
+            nodes.append(int(ttt[0]))
+
+    # Convert to numpy arrays.
+    triangle = np.asarray(triangles)
+    nodes = np.asarray(nodes)
+    X = np.asarray(x)
+    Y = np.asarray(y)
+    Z = np.asarray(z)
+
+    return(triangle, nodes, X, Y, Z)
+
+
+
+
 def plotUnstructuredGrid(triangles, nodes, x, y, z, colourLabel, addText=False, addMesh=False):
     """ 
     Takes the output of parseUnstructuredGridFVCOM() or 
@@ -92,7 +131,7 @@ def plotUnstructuredGrid(triangles, nodes, x, y, z, colourLabel, addText=False, 
 
     plt.figure()
     if z.max()-z.min() != 0:
-        plt.tripcolor(x, y, triangles, z, shading='interp')
+        plt.tripcolor(x, y, triangles, z, shading='faceted')
         cb = plt.colorbar()
         cb.set_label(colourLabel)
 
@@ -104,8 +143,10 @@ def plotUnstructuredGrid(triangles, nodes, x, y, z, colourLabel, addText=False, 
         for node in nodes:
             plt.text(x[node-1], y[node-1], str(nodes[node-1]),
                 horizontalalignment='center', verticalalignment='top', size=8)
-    plt.axes().set_aspect('equal', 'datalim')
-    #plt.clim(-1.5, 1.5)
+    plt.axes().set_aspect('equal')
+    plt.axes().autoscale(tight=True)
+    #plt.axis('tight')
+    plt.clim(-50, 0)
     #plt.title('Triplot of user-specified triangulation')
     plt.xlabel('Metres')
     plt.ylabel('Metres')
@@ -117,12 +158,18 @@ if __name__ == '__main__':
     
     from sys import argv
 
+    # A MIKE grid
+    [triangles, nodes, x, y, z] = parseUnstructuredGridMIKE('../data/csm_culver_v7.mesh')
     # An SMS grid
-    #[triangles, nodes, x, y, z] = parseUnstructuredGridSMS('tamar_co2V4.2dm')
-    # The model input grid
-    for grid in argv[1:]:
-        [triangles, nodes, x, y, z] = parseUnstructuredGridFVCOM(grid)
+    #[triangles, nodes, x, y, z] = parseUnstructuredGridSMS('../data/tamar_co2V4.2dm')
+    # An FVCOM grid
+    #[triangles, nodes, x, y, z] = parseUnstructuredGridFVCOM('../data/co2_grd.dat')
 
-        # Let's have a look-see
-        plotUnstructuredGrid(triangles, nodes, x, y, z, 'Depth (m)', True)
+    # Let's have a look-see
+    plotUnstructuredGrid(triangles, nodes, x, y, z, 'Depth (m)')
+
+    # Multiple grids
+    #for grid in argv[1:]:
+    #    [triangles, nodes, x, y, z] = parseUnstructuredGridFVCOM(grid)
+    #    plotUnstructuredGrid(triangles, nodes, x, y, z, 'Depth (m)', True)
 
