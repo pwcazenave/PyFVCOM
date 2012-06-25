992485b60e2514d07f4414fbb87a59f564991a78
Pierre Cazenave <pwcazenave@gmail.com>
2012-06-25 11:33:44 +0100

    Add new script to read the exported arcs from a MIKE Mesh Generator file and export an FVCOM CST file

---
+++ read_MIKE_coast.py
@@ -0,0 +1,78 @@
+
+
+
+def readArcMIKE(file, fileOut):
+    """ 
+    Read in a set of MIKE arc files and export to CST format compatible with 
+    FVCOM.
+
+    MIKE format is:
+
+        x, y, position, z(?), ID
+
+    where position is 1 = along arc and 0 = end of arc.
+    
+    CST (FVCOM) format is:
+
+      COAST
+      nArc
+      n z
+      x1 y1 z0
+      ...
+      xn yn zn
+
+    where nArc is the total number of arcs, n is the number of nodes in the
+    arc and z is a z value (typically zero, but we'll read it from the MIKE z
+    value. 
+
+    For the conversion, we don't need the ID, so we can ignore that. 
+
+    """
+    fileRead = open(file, 'r')
+    lines = fileRead.readlines()
+    fileRead.close()
+
+    fileWrite = open(fileOut, 'w')
+    # Add the easy header
+    fileWrite.write('COAST\n')
+    # This isn't pretty, but assuming you're coastline isn't millions of
+    # points, it should be ok...
+    nArcs = 0
+    for line in lines:
+        x, y, pos, z, ID = line.strip().split(' ')
+        if int(pos) == 0:
+            nArcs += 1
+
+    fileWrite.write('{}\n'.format(int(nArcs)))
+
+    arc = []
+    n = 1
+
+    for line in lines:
+        
+        x, y, pos, z, ID = line.strip().split(' ')
+        if int(pos) == 1:
+            arc.append([x, y])
+            n += 1
+        elif int(pos) == 0:
+            arc.append([x, y])
+            # We're at the end of an arc, so write out to file. Start with
+            # number of nodes and z
+            fileWrite.write('{}\t{}\n'.format(int(n), float(z)))
+            for arcPos in arc:
+                fileWrite.write('\t{}\t{}\t{}\n'.format(float(arcPos[0]), float(arcPos[1]), float(z)))
+            # Reset n and arc for new arc
+            n = 1
+            arc = []
+
+
+if __name__ == '__main__':
+
+    from os import path
+
+    infile = '../data/test.xyz'
+    infile = '../data/ukerc_shelf/ukerc/shelf_coast.xyz'
+    base, ext = path.splitext(infile)
+
+    readArcMIKE(infile, base + '.cst')
+
