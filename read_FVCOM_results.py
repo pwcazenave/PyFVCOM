fd95be0137f23643d99e49b2acdaf28a73e0ae43
Pierre Cazenave <pwcazenave@gmail.com>
2012-07-31 13:00:20 +0100

    Add function to extract surface elevation from a 2D array of surface elevations

--- read_FVCOM_results.py
+++ read_FVCOM_results.py
@@ -1,10 +1,11 @@
-from netCDF4 import Dataset, MFDataset
 
 def readFVCOM(file, varList, noisy=False):
     """
     Read in the FVCOM results file and spit out numpy arrays for
     each of the variables.
     """
+
+    from netCDF4 import Dataset, MFDataset
 
     rootgrp = Dataset(file, 'r')
     mfdata = MFDataset(file)
@@ -26,3 +27,23 @@ def readFVCOM(file, varList, noisy=False):
                 print
 
     return FVCOM
+
+def getSurfaceElevation(Z, idx):
+    """
+    Extract the surface elevation from Z at index ind. If ind is multiple
+    values, extract and return the surface elevations at all those locations.
+
+    Z is usually extracted from the dict created when using readFVCOM() on a
+    NetCDF file.
+
+    """
+
+    import numpy as np
+
+    nt, nx = np.shape(Z)
+
+    surfaceElevation = np.empty([nt,np.shape(idx)[0]])
+    for cnt, i in enumerate(idx):
+        surfaceElevation[:,cnt] = Z[:,i]
+
+    return surfaceElevation
