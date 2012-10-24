02f2fd408c4e1484653b566b8909c373a6d01ec1
Pierre Cazenave <pwcazenave@gmail.com>
2012-10-24 14:58:18 +0100

    First attempt at creating a setup file for more predictable installtion

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+classifiers = """\
+Development Status :: alpha
+Environment :: Console
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: GPL
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Scientific/Engineering
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+setup(
+    name = "fvcom-py",
+    version = "0.1",
+    author = "Pierre Cazenave",
+    author_email = "pica@pml.ac.uk",
+    description = ("fvcom-py is a collection of various tools and utilities which can be used to extract, analyse and plot input and output files from FVCOM."),
+    license = "GPL",
+    keywords = "fvcom",
+    platforms = "any",
+    url = "https://bitbucket.org/pwcazenave/fvcom-py",
+    packages=['fvcom-py'],
+    long_description=read('README.md'),
+    classifiers=classifiers
+)
+
