16351c7dfd4626f1fabdfe17d57f41333e8af04b
Pierre Cazenave <pica@pml.ac.uk>
2016-05-09 10:55:50 +0100

    Redo the setup.py for PyPI.

--- setup.py
+++ setup.py
@@ -1,33 +1,17 @@
-import os
-from setuptools import setup
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-classifiers = """\
-Development Status :: alpha
-Environment :: Console
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-License :: GPL
-Operating System :: OS Independent
-Programming Language :: Python
-Topic :: Scientific/Engineering
-Topic :: Software Development :: Libraries :: Python Modules
-"""
+from distutils.core import setup
 
 setup(
-    name = "PyFVCOM",
-    version = "1.2",
-    author = "Pierre Cazenave",
-    author_email = "pica@pml.ac.uk",
+    name = 'PyFVCOM',
+    packages = ['PyFVCOM'],
+    version = '1.2',
     description = ("PyFVCOM is a collection of various tools and utilities which can be used to extract, analyse and plot input and output files from FVCOM."),
-    license = "MIT",
-    keywords = "fvcom, unstructured grid, mesh",
-    platforms = "any",
-    url = "https://gitlab.ecosystem-modelling.pml.ac.uk/pica/PyFVCOM",
-    packages=['PyFVCOM'],
-    long_description=read('README.md'),
-    classifiers=classifiers
+    author = 'Pierre Cazenave',
+    author_email = 'pica@pml.ac.uk',
+    url = 'https://gitlab.ecosystem-modelling.pml.ac.uk/pica/PyFVCOM',
+    download_url = 'https://gitlab.ecosystem-modelling.pml.ac.uk/fvcom/PyFVCOM/tarball/1.2',
+    keywords = ['fvcom', 'unstructured grid', 'mesh'],
+    license = 'MIT',
+    platforms = 'any',
+    classifiers = []
 )
 
