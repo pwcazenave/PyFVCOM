210056a18b679ace55157008dbbf86cd1c67ee68
Pierre Cazenave <pica@pml.ac.uk>
2016-08-17 13:55:55 +0100

    Fix Python3 compatibility issue.

--- PyFVCOM/grid_tools.py
+++ PyFVCOM/grid_tools.py
@@ -905,7 +905,7 @@ def get_river_config(fileName, noisy=False, zeroindex=False):
 
     f.close()
 
-    if zeroindex and rivers.has_key('RIVER_GRID_LOCATION'):
+    if zeroindex and 'RIVER_GRID_LOCATION' in rivers:
         rivers['RIVER_GRID_LOCATION'] = [int(i) - 1 for i in rivers['RIVER_GRID_LOCATION']]
 
     return rivers
