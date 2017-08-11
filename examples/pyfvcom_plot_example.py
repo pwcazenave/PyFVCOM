318621493231ac232ad2954691a3c322a8de01c4
Pierre Cazenave <pica@pml.ac.uk>
2017-08-11 17:41:13 +0100

    Update the example to use the new Time class for the time series plots.

--- examples/pyfvcom_plot_example.py
+++ examples/pyfvcom_plot_example.py
@@ -10,7 +10,8 @@ get_ipython().magic('matplotlib inline')
 
 # Load an FVCOM model output and plot a surface.
 from PyFVCOM.read_results import FileReader
-from PyFVCOM.plot import Plotter
+from PyFVCOM.plot import Plotter, Time
+from PyFVCOM.grid_tools import make_water_column
 from cmocean import cm
 import matplotlib.pyplot as plt
 
@@ -35,10 +36,21 @@ plot.axes.set_title(fvcom.time.datetime[5].strftime('%Y-%m-%d %H:%M:%S'))
 # Do a time series at a specific location.
 gauge = (-5, 55)  # a sample (lon, lat) position
 index = fvcom.closest_node(gauge)
-fig = plt.figure(figsize(10, 4))
-ax = fig.add_subplot(111)
-ax.plot(fvcom.time.datetime, fvcom.data.zeta[:, index], 'r')
-ax.set_title('Surface elevation at {}, {}'.format(*gauge))
+time = Time(fvcom, figsize=(20, 9), title='Surface elevation at {}, {}'.format(*gauge))
+time.plot_line(fvcom.data.zeta[:, index], color='r')
+time.axes.set_ylabel('Surface elevation ({})'.format(fvcom.ds.variables['zeta'].units))
+
+
+# In[5]:
+
+# Plot a depth-varying profile through a water column
+fvcom = FileReader('sample.nc', variables=['temp', 'zeta'], dims={'time': [0, 400], 'node': [5000]})
+time = Time(fvcom, figsize=(25, 8), cb_label='Temperature $(^{\circ}C)$')
+z = make_water_column(fvcom.data.zeta, fvcom.grid.h, fvcom.grid.siglay)
+# fill_seabed makes the part of the plot below the seabed grey. 
+# We need to squeeze the data array since we've only extracted a single
+# position.
+time.plot_surface(z, np.squeeze(fvcom.data.temp), fill_seabed=True)
 
 
 # In[ ]:
