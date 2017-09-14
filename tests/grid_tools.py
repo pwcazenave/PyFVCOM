245605b56f6490e0ae44e790bb4c8e87bc7f2388
Pierre Cazenave <pica@pml.ac.uk>
2017-09-14 12:32:18 +0100

    Make the test grid marginally more complex and update the expected values accordingly.

--- tests/grid_tools.py
+++ tests/grid_tools.py
@@ -10,18 +10,19 @@ from PyFVCOM.grid_tools import get_area, node_control_area, element_control_area
 class GridToolsTest(TestCase):
 
     def setUp(self):
-        # Make a really simple unstructured grid of 4 elements stack on top of one another.
-        self.x = np.array([0, 1, 0, 1, 0, 1])
-        self.y = np.array([0, 0, 1, 1, 2, 2])
-        self.tri = np.array([[0, 2, 1], [1, 2, 3], [2, 5, 3], [2, 4, 5]])
+        """ Make a really simple unstructured grid of 8 elements as two rows of 4 elements. """
+        self.x = np.array([0, 1, 0, 1, 0, 1, 2, 2, 2])
+        self.y = np.array([0, 0, 1, 1, 2, 2, 0, 1, 2])
+        self.tri = np.array([[0, 2, 1], [1, 2, 3], [2, 5, 3], [2, 4, 5], [1, 3, 7], [1, 7, 6], [3, 5, 7], [7, 5, 8]])
         self.xc = nodes2elems(self.x, self.tri)
         self.yc = nodes2elems(self.y, self.tri)
+        self.z = np.array([0, 1, 1, 0, 2, 1, 2, 3, 3])
 
     def tearDown(self):
         pass
 
     def test_get_node_control_area(self):
-        test_node_area = 1 / 3
+        test_node_area = 2 / 3
         node = 1
         node_area = node_control_area(node, self.x, self.y, self.xc, self.yc, self.tri)
         test.assert_almost_equal(test_node_area, node_area)
@@ -36,6 +37,8 @@ class GridToolsTest(TestCase):
         test.assert_almost_equal(test_element_area, element_area)
 
     def test_get_control_volumes(self):
-        test_control_volumes = [1 / 3] * len(self.x)
-        test_control_volumes = [1] * len(self.x)
+        test_node_areas = [1 / 6, 2 / 3, 2 / 3,
+                           2 / 3, 1 / 6, 2 / 3,
+                           1 / 6, 2 / 3, 1 / 6]
+        test_element_areas = [0.5, 2, 2, 2, 0.5, 2, 0.5, 2, 0.5]
         node_areas, element_areas = control_volumes(self.x, self.y, self.tri)
