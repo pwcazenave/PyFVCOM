5f002d213d01a77445fb005a44bbfb86645ad96e
Pierre Cazenave <pica@pml.ac.uk>
2017-10-30 11:18:46 +0000

    Fix the name of the test class.

--- tests/utilities.py
+++ tests/utilities.py
@@ -10,7 +10,7 @@ from unittest import TestCase
 
 from PyFVCOM.utilities import *
 
-class StatsToolsTest(TestCase):
+class UtilitiesTest(TestCase):
 
     def setUp(self):
         """ Make a couple of time series """
