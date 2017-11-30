8ee0d33f73a994ef7a3ab1ab34cc68d80ca4420f
Pierre Cazenave <pica@pml.ac.uk>
2017-11-30 17:04:10 +0000

    Update to use the latest PyFVCOM utilities structure.

--- PyFVCOM/preproc.py
+++ PyFVCOM/preproc.py
@@ -32,7 +32,7 @@ from utide.utilities import Bunch
 from PyFVCOM.grid import Domain, grid_metrics, read_fvcom_obc, nodes2elems
 from PyFVCOM.grid import write_fvcom_mesh, connectivity, haversine_distance
 from PyFVCOM.grid import find_bad_node, get_attached_unique_nodes
-from PyFVCOM.utilities import date_range
+from PyFVCOM.utilities.time import date_range
 from PyFVCOM.read import FileReader
 from PyFVCOM.coordinate import utm_from_lonlat, lonlat_from_utm
 
