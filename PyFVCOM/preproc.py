606442ac29a0008ad8eb74f2fcfc05f8ba868c85
Pierre Cazenave <pica@pml.ac.uk>
2018-02-01 16:22:51 +0000

    Add debugging flag.

--- PyFVCOM/preproc.py
+++ PyFVCOM/preproc.py
@@ -59,6 +59,7 @@ class Model(Domain):
         super().__init__(*args, **kwargs)
 
         self.noisy = False
+        self.debug = True
         if 'noisy' in kwargs:
             self.noisy = kwargs['noisy']
 
