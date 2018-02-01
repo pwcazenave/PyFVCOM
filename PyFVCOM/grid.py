8d3de8b95ca20554e3f283543f2b9b7e0f5839a0
Pierre Cazenave <pica@pml.ac.uk>
2018-02-01 16:23:29 +0000

    Fix docstring.

--- PyFVCOM/grid.py
+++ PyFVCOM/grid.py
@@ -598,7 +598,7 @@ class OpenBoundary:
             lat and depth data as well as the time series to interpolate (4D volume [time, depth, lat, lon]).
         interval : str, optional
             Time sampling interval in days. Defaults to 1 day.
-        constrain_coordinates : bool
+        constrain_coordinates : bool, optional
             Set to True to constrain the open boundary coordinates (lon, lat, depth) to the supplied coarse data.
             This essentially squashes the open boundary to fit inside the coarse data and is, therefore, a bit of a
             fudge! Defaults to False.
