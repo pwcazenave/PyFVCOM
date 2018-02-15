8b8f1f4f21c52b3838fbe5ddf440d732f9bb6840
Pierre Cazenave <pica@pml.ac.uk>
2018-02-15 17:33:24 +0000

    Add functions to clean HTML markup.

--- PyFVCOM/utilities/general.py
+++ PyFVCOM/utilities/general.py
@@ -1,6 +1,10 @@
 import itertools
 import numpy as np
 import itertools
+from html.parser import HTMLParser
+from re import sub
+from sys import stderr
+from traceback import print_exc
 
 def fix_range(a, nmin, nmax):
     """
@@ -125,3 +129,53 @@ class ObjectFromDict:
         for key in kwargs:
             if key in keys:
                 setattr(self, key, kwargs[key])
+
+
+class _DeHTMLParser(HTMLParser):
+    """ Parse HTML so we can more easily extract sensible information from it. """
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.__text = []
+
+    def handle_data(self, data):
+        text = data.strip()
+        if len(text) > 0:
+            text = sub('[ \t\r\n]+', ' ', text)
+            self.__text.append(text + ' ')
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            self.__text.append('\n\n')
+        elif tag == 'br':
+            self.__text.append('\n')
+
+    def handle_startendtag(self, tag, attrs):
+        if tag == 'br':
+            self.__text.append('\n\n')
+
+    def text(self):
+        return ''.join(self.__text).strip()
+
+
+def cleanhtml(text):
+    """ Given some HTML text, remove the markup and leave only the text.
+
+    Parameters
+    ----------
+    text : str
+        The HTML to de-markup.
+
+    Returns
+    -------
+    cleaned : str
+        The cleaned text.
+
+    """
+    try:
+        parser = _DeHTMLParser()
+        parser.feed(text)
+        parser.close()
+        return parser.text()
+    except:
+        print_exc(file=stderr)
+        return text
