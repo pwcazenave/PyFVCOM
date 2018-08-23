Fix the calculation of the tidal axis (previously out by 90 degrees).

PyFVCOM/current.py
@@ -648,9 +648,8 @@ def principal_axis(u, v):
 
     # Rotation angle of major axis in radians relative to cartesian coordinates
     # ra = np.arctan2(V[0,1], V[1,1])
-    ra = atan2(V[0, 1], V[1, 1])
+    ra = atan2(V[1, 1], V[0, 1])
     # Express principal axis in compass coordinates.
-    # TR: still not sure here
     principal_axis = np.rad2deg(-ra)
     # Variance captured by principal axis.
     axis_variance = np.diag(lamb[0]) / np.trace(lamb)