0d30a22808423d73bc6c45b09404f835919670a2
Pierre Cazenave <pica@pml.ac.uk>
2018-09-06 16:42:54 +0100

    Fix error type.

--- PyFVCOM/read.py
+++ PyFVCOM/read.py
@@ -836,7 +836,7 @@ class FileReader(Domain):
                 print('Loading: {}'.format(v))
 
             if v not in self.ds.variables:
-                raise KeyError("Variable '{}' not present in {}".format(v, self._fvcom))
+                raise NameError("Variable '{}' not present in {}".format(v, self._fvcom))
 
             var_dim = self.ds.variables[v].dimensions
             variable_shape = self.ds.variables[v].shape
