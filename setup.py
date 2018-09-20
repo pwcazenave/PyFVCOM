7c113640fc3f9bbf169ebcd6918ffb1162916755
Pierre Cazenave <pica@pml.ac.uk>
2018-09-20 11:46:31 +0100

    Fix spacing in the setup.py script to conform to PEP8.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name = 'PyFVCOM',
-    packages = ['PyFVCOM'],
-    version = '2.1.0',
-    description = ("PyFVCOM is a collection of various tools and utilities which can be used to extract, analyse and plot input and output files from FVCOM."),
-    author = 'Pierre Cazenave',
-    author_email = 'pica@pml.ac.uk',
-    url = 'https://gitlab.ecosystem-modelling.pml.ac.uk/fvcom/PyFVCOM',
-    download_url = 'http://gitlab.em.pml.ac.uk/fvcom/PyFVCOM/repository/archive.tar.gz?ref=2.1.0',
-    keywords = ['fvcom', 'unstructured grid', 'mesh'],
-    license = 'MIT',
-    platforms = 'any',
-    install_requires = ['jdcal', 'lxml', 'matplotlib', 'netCDF4', 'networkx', 'numpy>=1.13.0', 'pandas', 'pyproj', 'pytz', 'scipy', 'pyshp', 'UTide', 'shapely'],
-    classifiers = []
+    name='PyFVCOM',
+    packages=['PyFVCOM'],
+    version='2.1.0',
+    description=("PyFVCOM is a collection of various tools and utilities which can be used to extract, analyse and plot input and output files from FVCOM."),
+    author='Pierre Cazenave',
+    author_email='pica@pml.ac.uk',
+    url='https://gitlab.ecosystem-modelling.pml.ac.uk/fvcom/PyFVCOM',
+    download_url='http://gitlab.em.pml.ac.uk/fvcom/PyFVCOM/repository/archive.tar.gz?ref=2.1.0',
+    keywords=['fvcom', 'unstructured grid', 'mesh'],
+    license='MIT',
+    platforms='any',
+    install_requires=['jdcal', 'lxml', 'matplotlib', 'netCDF4', 'networkx', 'numpy>=1.13.0', 'pandas', 'pyproj', 'pytz', 'scipy', 'pyshp', 'UTide', 'shapely'],
+    classifiers=[]
 )
 
