d6f6c4ef30c7a6206f130817a5f50b11ac477126
Pierre Cazenave <pica@pml.ac.uk>
2018-10-17 16:01:52 +0100

    Add new interactive animation class.

--- PyFVCOM/plot.py
+++ PyFVCOM/plot.py
@@ -1389,6 +1389,123 @@ class MPIWorker(object):
                                       dpi=120)
 
 
+class Player(FuncAnimation):
+    """ Animation class for FVCOM outputs. Shamelessly lifted from https://stackoverflow.com/a/46327978 """
+
+    def __init__(self, fig, func, init_func=None, fargs=None, save_count=None, mini=0, maxi=100, pos=(0.125, 0.92),
+                 **kwargs):
+        """
+        Initialise an animation window.
+
+        Parameters
+        ----------
+        fig : matplotlib.figure.Figure
+            The figure into which we should animate.
+        func : function
+            The function describing the animation.
+        init_func : function, optional
+            An initial function for the first frame.
+        fargs : tuple or None, optional
+           Additional arguments to pass to each call to ``func``
+        save_count : int, optional
+           The number of values from `frames` to cache.
+        mini : int, optional
+            The start index for the animation (defaults to zero).
+        maxi : int, optional
+            The maximum index for the animation (defaults to 100).
+        pos : tuple
+            The (x, y) position of the player controls. Defaults to near the top of the figure.
+
+        Additional kwargs are passed to `matplotlib.animation.FuncAnimation'.
+
+        """
+
+        self.i = 0
+        self.min = mini
+        self.max = maxi
+        self.runs = True
+        self.forwards = True
+        self.fig = fig
+        self.func = func
+        self.setup(pos)
+
+        FuncAnimation.__init__(self, self.fig, self.func, frames=self.play(), init_func=init_func, fargs=fargs,
+                               save_count=save_count, **kwargs)
+
+    def play(self):
+        while self.runs:
+            self.i = self.i+self.forwards-(not self.forwards)
+            if self.min < self.i < self.max:
+                yield self.i
+            else:
+                self.stop()
+                yield self.i
+
+    def start(self):
+        self.runs=True
+        self.event_source.start()
+
+    def stop(self):
+        self.runs = False
+        self.event_source.stop()
+
+    def forward(self):
+        self.forwards = True
+        self.start()
+
+    def backward(self):
+        self.forwards = False
+        self.start()
+
+    def oneforward(self):
+        self.forwards = True
+        self.onestep()
+
+    def onebackward(self):
+        self.forwards = False
+        self.onestep()
+
+    def onestep(self):
+        if self.min < self.i < self.max:
+            self.i = self.i + self.forwards - (not self.forwards)
+        elif self.i == self.min and self.forwards:
+            self.i += 1
+        elif self.i == self.max and not self.forwards:
+            self.i -= 1
+        self.func(self.i)
+        self.slider.set_val(self.i)
+        self.fig.canvas.draw_idle()
+
+    def setup(self, pos):
+        playerax = self.fig.add_axes([pos[0],pos[1], 0.64, 0.04])
+        divider = mpl_toolkits.axes_grid1.make_axes_locatable(playerax)
+        bax = divider.append_axes("right", size="80%", pad=0.05)
+        sax = divider.append_axes("right", size="80%", pad=0.05)
+        fax = divider.append_axes("right", size="80%", pad=0.05)
+        ofax = divider.append_axes("right", size="100%", pad=0.05)
+        sliderax = divider.append_axes("right", size="500%", pad=0.07)
+        self.button_oneback = matplotlib.widgets.Button(playerax, label=u'$\u29CF$')
+        self.button_back = matplotlib.widgets.Button(bax, label=u'$\u25C0$')
+        self.button_stop = matplotlib.widgets.Button(sax, label=u'$\u25A0$')
+        self.button_forward = matplotlib.widgets.Button(fax, label=u'$\u25B6$')
+        self.button_oneforward = matplotlib.widgets.Button(ofax, label=u'$\u29D0$')
+        self.button_oneback.on_clicked(self.onebackward)
+        self.button_back.on_clicked(self.backward)
+        self.button_stop.on_clicked(self.stop)
+        self.button_forward.on_clicked(self.forward)
+        self.button_oneforward.on_clicked(self.oneforward)
+        self.slider = matplotlib.widgets.Slider(sliderax, '',
+                                                self.min, self.max, valinit=self.i)
+        self.slider.on_changed(self.set_pos)
+
+    def set_pos(self, i):
+        self.i = int(self.slider.val)
+        self.func(self.i)
+
+    def update(self,i):
+        self.slider.set_val(i)
+
+
 def plot_domain(domain, mesh=False, depth=False, **kwargs):
     """
     Add a domain plot to the given domain (as domain.domain_plot).
