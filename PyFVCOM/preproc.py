487d52c1325d975153202aafef0fb04609c95305
Modellers Operational <modop@phorcys.npm.ac.uk>
2019-01-28 16:49:31 +0000

    And in the preproc interpolation code. These should probably be consolidated some time

--- PyFVCOM/preproc.py
+++ PyFVCOM/preproc.py
@@ -4582,7 +4582,7 @@ class Restart(FileReader):
 
             if np.any(internal_points):
                 xv, yv = np.meshgrid(coarse.grid.lon, coarse.grid.lat)
-                valid_ll = np.asarray([xv[~land_mask], yv[~land_mask]]).T
+                valid_ll = np.asarray([x[~internal_points], y[~internal_points]]).T
                 for this_ind in np.where(internal_points)[0]:
                     nearest_valid_ind = np.argmin((valid_ll[:,0] - x[this_ind])**2 + (valid_ll[:,1] - y[this_ind])**2)
                     x[this_ind] = valid_ll[nearest_valid_ind,0]
