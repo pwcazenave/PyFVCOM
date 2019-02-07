5e3d56bdf7c9757b8b8a1286c485f258139656cc
Pierre Cazenave <pica@pml.ac.uk>
2019-02-07 10:23:37 +0000

    The function to add UGRID support to FVCOM outputs is now in PyFVCOM.grid.

--- PyFVCOM/utilities/grid.py
+++ PyFVCOM/utilities/grid.py
@@ -8,25 +8,3 @@ from PyFVCOM.grid import nodes2elems, element_side_lengths, unstructured_grid_de
 from PyFVCOM.utilities.time import date_range
 
 
-def fvcom2ugrid(fvcom):
-    """
-    Add the necessary information to convert an FVCOM output file to one which is compatible with the UGRID format.
-
-    Parameters
-    ----------
-    fvcom : str
-        Path to an FVCOM netCDF file (can be a remote URL).
-
-    """
-
-    with Dataset(fvcom, 'a') as ds:
-        fvcom_mesh = ds.createVariable('fvcom_mesh', np.int32)
-        setattr(fvcom_mesh, 'cf_role', 'mesh_topology')
-        setattr(fvcom_mesh, 'topology_dimension', 2)
-        setattr(fvcom_mesh, 'node_coordinates', 'lon lat')
-        setattr(fvcom_mesh, 'face_coordinates', 'lonc latc')
-        setattr(fvcom_mesh, 'face_node_connectivity', 'nv')
-
-        # Add the global convention.
-        setattr(ds, 'Convention', 'UGRID-1.0')
-        setattr(ds, 'CoordinateProjection', 'none')
