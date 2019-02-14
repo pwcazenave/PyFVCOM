8097f6058af2bc08e3609d445b0eefe099fc1ded
Pierre Cazenave <pica@pml.ac.uk>
2019-02-14 11:29:14 +0000

    Add missing import.

--- PyFVCOM/__init__.py
+++ PyFVCOM/__init__.py
@@ -27,6 +27,7 @@ from PyFVCOM import tide
 from PyFVCOM import plot
 from PyFVCOM import preproc
 from PyFVCOM import read
+from PyFVCOM import utilities
 from PyFVCOM import validation
 
 if sys.version_info.major < 3 and sys.version_info.minor < 6:
