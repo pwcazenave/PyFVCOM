a681dbb34dda7544cab8357142139448290d78ec
Mike Bedington <mbe@pml.ac.uk>
2019-03-13 14:31:37 +0000

    Fix phase interpolation in add_tpxo_tide

--- PyFVCOM/grid/_grid.py
+++ PyFVCOM/grid/_grid.py
@@ -36,7 +36,7 @@ from utide.utilities import Bunch
 
 from PyFVCOM.coordinate import utm_from_lonlat, lonlat_from_utm
 from PyFVCOM.ocean import zbar
-from PyFVCOM.utilities.general import PassiveStore, fix_range
+from PyFVCOM.utilities.general import PassiveStore, fix_range, cart2pol, pol2cart
 from PyFVCOM.utilities.time import date_range
 
 
@@ -1393,11 +1393,14 @@ class OpenBoundary(object):
             harmonics_lon = np.unique(harmonics_lon)
             harmonics_lat = np.unique(harmonics_lat)
 
+        # Since interpolating phase directly is a bad idea (cos of the 360 -> 0 degree thing) convert to vectors first
+        harmonics_u, harmonics_v = pol2cart(amp_data, phase_data, degrees=True) 
+
         # Make a dummy first dimension since we need it for the RegularGridInterpolator but don't actually
         # interpolated along it.
         c_data = np.arange(amp_data.shape[0])
-        amplitude_interp = RegularGridInterpolator((c_data, harmonics_lon, harmonics_lat), amp_data, method=interp_method, fill_value=None)
-        phase_interp = RegularGridInterpolator((c_data, harmonics_lon, harmonics_lat), phase_data, method=interp_method, fill_value=None)
+        u_interp = RegularGridInterpolator((c_data, harmonics_lon, harmonics_lat), harmonics_u, method=interp_method, fill_value=None)
+        v_interp = RegularGridInterpolator((c_data, harmonics_lon, harmonics_lat), harmonics_v, method=interp_method, fill_value=None)
 
         # Fix our input position longitudes to be in the 0-360 range to match the harmonics data range,
         # if necessary.
@@ -1415,8 +1418,11 @@ class OpenBoundary(object):
         xx = np.tile(x, [amp_data.shape[0], 1])
         yy = np.tile(y, [amp_data.shape[0], 1])
         ccidx = np.tile(c_data, [len(x), 1]).T
-        amplitudes = amplitude_interp((ccidx, xx, yy)).T
-        phases = phase_interp((ccidx, xx, yy)).T
+        u = u_interp((ccidx, xx, yy)).T
+        v = v_interp((ccidx, xx, yy)).T
+
+        # And convert back to amplitude and phase
+        amplitudes, phases = cart2pol(u, v, degrees=True)        
 
         return amplitudes, phases
 
