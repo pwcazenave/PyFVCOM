Fix generalised sigma coordinate calculation. This is potentially a big bug fix as it looks like the add_sigma was erroneously using the tanh vertical levels for any area with h>h_min before this...

PyFVCOM/preproc.py
@@ -915,7 +915,7 @@ class Model(Domain):
             sigma_levels = np.empty((self.dims.node, nlev)) * np.nan
             for i in range(self.dims.node):
                 sigma_levels[i, :] = self.sigma_generalized(
-                        nlev, dl, du, self.grid.h[i], min_constant_depth)
+                        nlev, dl, du, self.grid.h[i], min_constant_depth, kl, ku)
         elif sigtype.lower() == 'uniform':
             sigma_levels = np.tile(self.sigma_geometric(nlev, 1), 
                     (self.dims.node, 1))
@@ -986,7 +986,7 @@ class Model(Domain):
         # Update the open boundaries.
         self._update_open_boundaries()
 
-    def sigma_generalized(self, levels, dl, du, h, hmin):
+    def sigma_generalized(self, levels, dl, du, h, hmin, kl, ku):
         """
         Generate a generalised sigma coordinate distribution.
 
@@ -1002,6 +1002,10 @@ class Model(Domain):
             Water depth (positive down).
         hmin : float
             Minimum water depth (positive down).
+        ku : int
+            Number of levels in upper boundary.
+        kl : int
+            Number of levels in lower boundary.
 
         Returns
         -------
@@ -1014,9 +1018,32 @@ class Model(Domain):
         h = np.abs(h)
         hmin = np.abs(hmin)
 
+        # Check that the uniform --> generalised transition makes sense
+        if not hmin/(levels-1) == dl/kl or not hmin/(levels-1) == du/ku:
+            raise ValueError ('Uniform to generalised sigma layers are not matched')
+
         if h > hmin:
-            # Hyperbolic tangent for deep areas
-            dist = self.sigma_tanh(levels, dl, du)
+            # Fixed layers for the top and bottom, can assume they are the same thickness as this
+            # is checked above
+            perc_in_boundary = (dl + du)/h
+            layers_in_boundary = ku+kl
+            perc_per_layer_boundary = perc_in_boundary/layers_in_boundary 
+
+            # Uniform in between
+            perc_in_midlayer = (h - (dl+du)*(1-1/layers_in_boundary))/h # how much of the water column still to divvy up
+            layers_in_mid = levels - (ku + kl)
+            perc_per_layer_mid = perc_in_midlayer/layers_in_mid
+            
+            # Compile it into one set of dists
+            dist = [0]
+            for i in np.arange(1,ku):
+                dist.append(dist[-1] + perc_per_layer_boundary)
+            for i in np.arange(0, layers_in_mid):
+                dist.append(dist[-1] + perc_per_layer_mid)
+            for i in np.arange(0, kl):
+                dist.append(dist[-1] + perc_per_layer_boundary)
+            dist = np.asarray(dist)
+            
         else:
             # Uniform for shallow areas
             dist = self.sigma_geometric(levels, 1)